a293128d37f265a1dd2917fc6205b8448d1948ab
HuongTra1105 <huongtraungnguyen1105@gmail.com>
2025-09-27 21:29:34 +0700

    lan 1

---
+++ ManHinhUngDung/app/src/main/java/com/example/manhinhungdung/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.manhinhungdung;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.manhinhungdung.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+
+}
